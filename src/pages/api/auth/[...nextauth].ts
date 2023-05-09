8950e36917477471122b4628316a0025df58b43b
Kenzo Wada <used.acormhib@gmail.com>
2023-05-09 09:43:54 +0900

    :bug: fix bugs

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,31 +8,31 @@ const prisma = new PrismaClient;
 
 export default NextAuth({
   providers: [
-    CredentialsProvider({
-      // name: "",
-      credentials: {
-        username: { label: "ユーザー名", type: "text", placeholder: "ユーザー名" },
-        password: {  label: "パスワード", type: "password" }
-      },
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      async authorize(credentials, req) {
-        const username = credentials?.username
-        const password = credentials?.password
+    // CredentialsProvider({
+    //   // name: "",
+    //   credentials: {
+    //     username: { label: "ユーザー名", type: "text", placeholder: "ユーザー名" },
+    //     password: {  label: "パスワード", type: "password" }
+    //   },
+    //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    //   async authorize(credentials, req) {
+    //     const username = credentials?.username
+    //     const password = credentials?.password
 
-        // データベースからユーザー情報を取得
-        const user = await prisma.user.findUnique({
-          where: { username: username },
-        })
+    //     // データベースからユーザー情報を取得
+    //     const user = await prisma.user.findUnique({
+    //       where: { username: username },
+    //     })
 
-        if (user && user.password === password) {
-          // パスワードが一致した場合、ユーザーオブジェクトを返す
-          return { id: user.id, name: user.name, email: user.email }
-        } else {
-          // 一致しない場合、NULLを返す
-          return null
-        }
-      }
-    }),
+    //     if (user && user.password === password) {
+    //       // パスワードが一致した場合、ユーザーオブジェクトを返す
+    //       return { id: user.id, name: user.name, email: user.email }
+    //     } else {
+    //       // 一致しない場合、NULLを返す
+    //       return null
+    //     }
+    //   }
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
