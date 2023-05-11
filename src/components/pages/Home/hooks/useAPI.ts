69b40658478f33716f302b422d89ab9bd330f95a
Kenzo Wada <used.acormhib@gmail.com>
2023-05-11 11:03:26 +0900

    :bug: bug fix

--- src/components/pages/Home/hooks/useAPI.ts
+++ src/components/pages/Home/hooks/useAPI.ts
@@ -1,17 +1,8 @@
+import type { Post as PostType } from '@prisma/client';
 import { useEffect, useState } from 'react';
 
 interface HooksType {
-  posts: {
-    id: string;
-    title: string | null;
-    content: string | null;
-    image: string | null;
-    published: boolean;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at: Date | null;
-    authorId: string;
-  }[];
+  posts: PostType[];
 }
 
 const useAPI = (): HooksType => {
